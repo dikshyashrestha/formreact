4243138e5484ad39384c42073ebfc1cf814c17c1
dikshyashrestha <shresthadikshya58@gmail.com>
2021-03-04 15:35:01 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './style.css'
+
+class Form extends Component{
+  render(){
+    return(
+    <div className = 'hello'>
+      <h1>Please fill up the form</h1>
+      
+      <input
+        className = 'field'
+        type = 'name'
+        placeholder = 'name'
+      />
+      <br/>
+
+      <input
+        className = 'field'
+        type = 'address'
+        placeholder  = 'address'
+        />
+
+      <br/>
+
+      <input
+        className = 'field'
+        type = 'contact'
+        placeholder = 'contact'
+       />
+      <br/>
+
+      <input
+        className = 'field'
+        type = 'emali'
+        placeholder = 'email'
+      />
+      <br/>
+      <button className = 'field'>
+        Submit
+      </button>
+      
+    </div>
+     )
+  }
+}
+export default Form;
